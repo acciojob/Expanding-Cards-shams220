b8049dd85bc21f8ab7dd591f597165b1facea538
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-25 21:48:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const images = document.querySelectorAll("img");
+      const texts = document.querySelectorAll(".img-text");
+
+       texts.forEach((t) => {
+        t.style.visibility = "hidden";
+      });
+      images.forEach((i,index) => {
+        i.addEventListener("mouseover", () => {
+          i.style.width = "500px";
+        // text[index].style.visibility = "visible"
+          texts[index].style.visibility = "visible";
+          
+        });
+        i.addEventListener("mouseout", () => {
+          i.style.width = "100px";
+          texts[index].style.visibility = "hidden";
+        });
+      });
